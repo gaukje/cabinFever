9d803389eab4ae6e9da8eb3a05f6521460c50e90
gautte <s362066@oslomet.no>
2023-08-31 11:39:05 +0200

    Add project files.

---
+++ HouseRentingSystemSLAyer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseRentingSystemSLAyer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
