1fc90dce2b717e153c333dacfa77c0512a1535de
Jakob Anderson <jakob.anderson.work@gmail.com>
2023-08-31 13:17:52 +0200

    Update Item.cs

--- HouseRentingSystemSLAyer/Models/Item.cs
+++ HouseRentingSystemSLAyer/Models/Item.cs
@@ -2,7 +2,7 @@
 {
     public class Item
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public decimal PricePerNight { get; set; }
         public int Capacity { get; set; }
