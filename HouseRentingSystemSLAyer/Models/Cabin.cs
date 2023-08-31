9fc2142bca12a04283e4d8cbc8c883a4da080921
gautte <s362066@oslomet.no>
2023-08-31 12:08:55 +0200

    Lagd klasse for hytte under Models mappen

---
+++ HouseRentingSystemSLAyer/Models/Cabin.cs
@@ -0,0 +1,14 @@
+﻿namespace HouseRentingSystemSLAyer.Models
+{
+    public class Cabin
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public decimal PricePerNight { get; set; }
+        public int Capacity { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public string? ImageUrl { get; set; }
+        public Boolean? IsAvailable { get; set; }
+    }
+}
