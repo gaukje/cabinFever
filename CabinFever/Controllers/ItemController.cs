b0abb93b1a12ae1c8029c08e3045af1da4b14903
Diba00 <dibas.shishegar@gmail.com>
2023-09-11 16:17:12 +0200

    table og grid view

--- CabinFever/Controllers/ItemController.cs
+++ CabinFever/Controllers/ItemController.cs
@@ -1,12 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CabinFever.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace CabinFever.Controllers
+namespace CabinFever.Controllers;
+
+public class ItemController : Controller
 {
-    public class ItemController : Controller
+    public IActionResult Table()
     {
-        public IActionResult Index()
+        var items = GetItems();
+        ViewBag.CurrentViewName = "Table";
+        return View(items);
+    }
+
+    public IActionResult Grid()
+    {
+        var items = GetItems();
+        ViewBag.CurrentViewName = "Grid";
+        return View(items);
+    }
+
+    public List<Item> GetItems()
+    {
+        var items = new List<Item>();
+        var item1 = new Item
         {
-            return View();
-        }
+            Id = 1,
+            Name = "Fjellslott",
+            PricePerNight = 2100,
+            Capacity = 4,
+            Description = "An amazing cabin at Fjellslott",
+            ImageUrl = "/images/hytte_stock_1.jpg"
+        };
+
+        var item2 = new Item
+        {
+            Id = 2,
+            Name = "Kragerø",
+            PricePerNight = 3500,
+            Capacity = 5,
+            Description = "This cabin is gonna blow you away",
+            ImageUrl = "/images/hytte_stock_2.jpg"
+        };
+
+        var item3 = new Item
+        {
+            Id = 3,
+            Name = "Lofoten",
+            PricePerNight = 4300,
+            Capacity = 6, 
+            Description = "Once in a lifetime cabin experience in Lofoten",
+            ImageUrl = "/images/hytte_stock_3.jpg"
+        };
+
+        items.Add(item1);
+        items.Add(item2);
+        items.Add(item3);
+        return items;
     }
 }
