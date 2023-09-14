f03efdc2f1fd75d3892d6a6374b3e6ae0f6994ad
Jakob Anderson <jakob.anderson.work@gmail.com>
2023-09-14 13:24:23 +0200

    Logg inn eller Min side check på navbar

---
+++ CabinFever/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CabinFever.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult LoggInn()
+        {
+            return View();
+        }
+
+        public IActionResult Registrer()
+        {
+            return View();
+        }
+    }
+}
