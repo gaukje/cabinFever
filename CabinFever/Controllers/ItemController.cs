af833dfa8b662bc39173b7ac0ba2d2c76d102bca
KAGrove <knut.a.grove@gmail.com>
2023-09-18 15:46:26 +0200

    Gjort om til Asynchronous Database Communication

--- CabinFever/Controllers/ItemController.cs
+++ CabinFever/Controllers/ItemController.cs
@@ -5,19 +5,11 @@ using System.Threading.Tasks;
 using CabinFever.Models;
 using Microsoft.AspNetCore.Mvc;
 using CabinFever.ViewModels;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace CabinFever.Controllers;
-
 public class ItemController : Controller
 {
-    //public IActionResult Table()
-    //{
-    //var items = GetItems();
-    //ViewBag.CurrentViewName = "Table";
-    //return View(items);
-    //}
-
     private readonly ItemDbContext _itemDbContext;
 
     public ItemController(ItemDbContext itemDbContext)
@@ -25,49 +17,50 @@ public class ItemController : Controller
         _itemDbContext = itemDbContext;
     }
 
-    public IActionResult Grid()
+    public async Task<IActionResult> Grid()
     {
-        List<Item> items = _itemDbContext.Items.ToList();
+        List<Item> items = await _itemDbContext.Items.ToListAsync();
         var itemListViewModel = new ItemListViewModel(items, "Grid");
         return View(itemListViewModel);
     }
 }
 
-    //public List<Item> GetItems()
-    //{
-    //    var items = new List<Item>();
-    //    var item1 = new Item
-    //    {
-    //        Id = 1,
-    //        Name = "Fjellslott",
-    //        PricePerNight = 2100,
-    //        Capacity = 4,
-    //        Description = "An amazing cabin at Fjellslott",
-    //        ImageUrl = "/images/hytte_stock_1.jpg"
-    //    };
 
-    //    var item2 = new Item
-    //    {
-    //        Id = 2,
-    //        Name = "Kragerø",
-    //        PricePerNight = 3500,
-    //        Capacity = 5,
-    //        Description = "This cabin is gonna blow you away",
-    //        ImageUrl = "/images/hytte_stock_2.jpg"
-    //    };
+//public List<Item> GetItems()
+//{
+//    var items = new List<Item>();
+//    var item1 = new Item
+//    {
+//        Id = 1,
+//        Name = "Fjellslott",
+//        PricePerNight = 2100,
+//        Capacity = 4,
+//        Description = "An amazing cabin at Fjellslott",
+//        ImageUrl = "/images/hytte_stock_1.jpg"
+//    };
 
-    //    var item3 = new Item
-    //    {
-    //        Id = 3,
-    //        Name = "Lofoten",
-    //        PricePerNight = 4300,
-    //        Capacity = 6, 
-    //        Description = "Once in a lifetime cabin experience in Lofoten",
-    //        ImageUrl = "/images/hytte_stock_3.jpg"
-    //    };
+//    var item2 = new Item
+//    {
+//        Id = 2,
+//        Name = "Kragerø",
+//        PricePerNight = 3500,
+//        Capacity = 5,
+//        Description = "This cabin is gonna blow you away",
+//        ImageUrl = "/images/hytte_stock_2.jpg"
+//    };
 
-    //    items.Add(item1);
-    //    items.Add(item2);
-    //    items.Add(item3);
-    //    return items;
-    //}
+//    var item3 = new Item
+//    {
+//        Id = 3,
+//        Name = "Lofoten",
+//        PricePerNight = 4300,
+//        Capacity = 6, 
+//        Description = "Once in a lifetime cabin experience in Lofoten",
+//        ImageUrl = "/images/hytte_stock_3.jpg"
+//    };
+
+//    items.Add(item1);
+//    items.Add(item2);
+//    items.Add(item3);
+//    return items;
+//}
