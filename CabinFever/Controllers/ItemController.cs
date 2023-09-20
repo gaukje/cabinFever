c746f78452be61801b5e3795abbe3b55d917a7ee
Diba00 <dibas.shishegar@gmail.com>
2023-09-20 15:04:10 +0200

    Lagt til log

--- CabinFever/Controllers/ItemController.cs
+++ CabinFever/Controllers/ItemController.cs
@@ -3,19 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 using CabinFever.ViewModels;
 using CabinFever.Models;
 
+
 namespace CabinFever.Controllers;
 
 public class ItemController : Controller
 {
     private readonly IItemRepository _itemRepository;
+    private readonly ILogger<ItemController> _logger;
 
-    public ItemController(IItemRepository itemRepository)
+    public ItemController(IItemRepository itemRepository, ILogger<ItemController>
+        logger)
     {
         _itemRepository = itemRepository;
+        _logger = logger;
     }
 
     public async Task<IActionResult> Grid()
     {
+        _logger.LogInformation("This is an information message.");
+        _logger.LogWarning("This is a warning message.");
+        _logger.LogError("This is an error message.");
+
         var items = await _itemRepository.GetAll();
         var itemListViewModel = new ItemListViewModel(items, "Grid");
         return View(itemListViewModel);
--- CabinFever/Program.cs
+++ CabinFever/Program.cs
@@ -1,6 +1,7 @@
 using CabinFever.Models;
 using Microsoft.EntityFrameworkCore;
 using CabinFever.DAL;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,17 @@ builder.Services.AddDbContext<ItemDbContext>(options =>
 });
 
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
+
+var loggerConfiguration = new LoggerConfiguration()
+    .MinimumLevel.Information() // nivåer: trace, info, warning, error, fatal
+    .WriteTo.File($"Logs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log");
+
+loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
+                            e.Level == Serilog.Events.LogEventLevel.Information &&
+                            e.MessageTemplate.Text.Contains("Executed DbCommand"));
+
+var logger = loggerConfiguration.CreateLogger();
+builder.Logging.AddSerilog(logger);
 
 var app = builder.Build();
 
