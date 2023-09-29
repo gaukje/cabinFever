269157eb6b2b2713f4cda9ba3b5ca61e40064ef9
gautte <s362066@oslomet.no>
2023-09-29 13:59:15 +0200

    Endret 'Id' på Item til å være 'ItemId' Lettere å holde oversikt

--- CabinFever/Controllers/OrderController.cs
+++ CabinFever/Controllers/OrderController.cs
@@ -33,8 +33,8 @@ public class OrderController : Controller
 
             ItemSelectList = items.Select(item => new SelectListItem
             {
-                Value = item.Id.ToString(),
-                Text = item.Id.ToString() + ": " + item.Name
+                Value = item.ItemId.ToString(),
+                Text = item.ItemId.ToString() + ": " + item.Name
             }).ToList(),
 
             OrderSelectList = orders.Select(order => new SelectListItem 
