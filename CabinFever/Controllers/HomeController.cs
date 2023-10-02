5b11efa4b23a5bc3f59d0f0ed27218193e6c42f2
KAGrove <knut.a.grove@gmail.com>
2023-10-02 11:05:52 +0200

    Table vises på Minside (foreløpig absolutt alle hyttene)

--- CabinFever/Controllers/HomeController.cs
+++ CabinFever/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 ﻿using CabinFever.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging; // Husk å legge til denne importen for ILogger
 using System.Diagnostics;
+using System.Threading.Tasks; // Husk å legge til denne importen for Task
+using CabinFever.DAL; // Husk å legge til denne importen for ItemRepository
+using CabinFever.ViewModels; // Husk å legge til denne importen for ItemListViewModel
+using System.Linq; // Husk å legge til denne importen for Enumerable.Empty
 
 namespace CabinFever.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IItemRepository _itemRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IItemRepository itemRepository)
         {
             _logger = logger;
+            _itemRepository = itemRepository;
         }
 
         public IActionResult Index()
@@ -27,10 +34,12 @@ namespace CabinFever.Controllers
         {
             return View();
         }
-        public IActionResult MinSide()
-        {
 
-            return View();
+        public async Task<IActionResult> MinSide() // Gjør denne metoden asynkron
+        {
+            var items = await _itemRepository.GetAll(); // Hent alle items fra databasen
+            var model = new ItemListViewModel(items ?? Enumerable.Empty<Item>(), "MinSide"); // Send items til visningen
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
