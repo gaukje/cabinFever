69fefbb52c93d7c2816edddccd1c938077fa0101
gautte <s362066@oslomet.no>
2023-10-12 15:55:04 +0200

    Prøver å gjør sånn at bruker blir redirected til en annen side etter å ha logget inn etter reservering. Pusher for å evt bruke i fremtiden

--- CabinFever/Controllers/OrderController.cs
+++ CabinFever/Controllers/OrderController.cs
@@ -40,48 +40,51 @@ namespace CabinFever.Controllers
             // Hent UserId
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            // Sett UserId på order
-            order.UserId = userId;
-
-            var itemId = Request.Form["ItemId"];
-            order.ItemId = Convert.ToInt32(itemId);
-            _logger.LogInformation("ItemId from form: {ItemId}", itemId);
-
-            // Log the order state
-            _logger.LogInformation("Order before saving: {@Order}", order);
-
-            // Oppdater ModelState manuelt
-            ModelState.Clear();
-            TryValidateModel(order);
-
-            if (!ModelState.IsValid)
+            if (userId != null)
             {
-                foreach (var state in ModelState)
+                // Sett UserId på order
+                order.UserId = userId;
+
+                // Log the order state
+                _logger.LogInformation("Order before saving: {@Order}", order);
+
+                // Oppdater ModelState manuelt
+                ModelState.Clear();
+                TryValidateModel(order);
+
+                if (!ModelState.IsValid)
                 {
-                    foreach (var error in state.Value.Errors)
+                    foreach (var state in ModelState)
                     {
-                        _logger.LogError("Model validation error for {Key}: {ErrorMessage}", state.Key, error.ErrorMessage);
+                        foreach (var error in state.Value.Errors)
+                        {
+                            _logger.LogError("Model validation error for {Key}: {ErrorMessage}", state.Key, error.ErrorMessage);
+                        }
                     }
+
+                    // Return the view with the model to display validation error messages
+                    return View(order);
                 }
 
-                // Return the view with the model to display validation error messages
-                return View(order);
-            }
+                try
+                {
+                    _itemDbContext.Orders.Add(order);
+                    await _itemDbContext.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    // Log any exception that occurs
+                    _logger.LogError(ex, "Error occurred while saving order: {@Order}", order);
+                    throw; // Re-throw the exception to be handled by the middleware or for further debugging
+                }
 
-            try
-            {
-                _itemDbContext.Orders.Add(order);
-                await _itemDbContext.SaveChangesAsync();
+                // Redirect to another action as per your flow
+                return RedirectToAction("Index", "Home");
             }
-            catch (Exception ex)
+            else
             {
-                // Log any exception that occurs
-                _logger.LogError(ex, "Error occurred while saving order: {@Order}", order);
-                throw; // Re-throw the exception to be handled by the middleware or for further debugging
+                return RedirectToAction("MinSide", "HomeController");
             }
-
-            // Redirect to another action as per your flow
-            return RedirectToAction("Index", "Home");
         }
 
 
